c37e3f7f33915d539da369f5467b8bb5594a4416
Nat Welch <nat@natwelch.com>
2014-10-18 13:26:12 +0000

    normalize phone numbers first

--- app.rb
+++ app.rb
@@ -63,8 +63,9 @@ end
 # Takes in a phone number and converts it to E164, the international phone
 # number format.
 def convert_to_e164 raw_phone
+  nrml = Phony.normalize(raw_phone)
   return Phony.format(
-    raw_phone,
+    nrml,
     :format => :international,
     :spaces => ''
   ).gsub(/\s+/, "") # Phony won't remove all spaces
