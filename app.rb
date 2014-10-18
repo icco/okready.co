6b68c703c5adf9edb73e4c6b97587434d796d714
Nat Welch <nat@natwelch.com>
2014-10-18 08:51:06 +0000

    dat env

--- app.rb
+++ app.rb
@@ -1,2 +1,7 @@
-require "sinatra"
+# Welcome to OK Ready
 
+# App Setup - Gems, etc.
+RACK_ENV ||= ENV['RACK_ENV'] ||= 'development' unless defined?(RACK_ENV)
+require 'rubygems' unless defined?(Gem)
+require 'bundler/setup'
+Bundler.require(:default, RACK_ENV)
