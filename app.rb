44cf3fae1aca702128f6966d59c786f2a683206d
Nat Welch <nat@natwelch.com>
2014-10-18 11:53:33 +0000

    Very basic three handlers

--- app.rb
+++ app.rb
@@ -5,3 +5,25 @@ RACK_ENV ||= ENV['RACK_ENV'] ||= 'development' unless defined?(RACK_ENV)
 require 'rubygems' unless defined?(Gem)
 require 'bundler/setup'
 Bundler.require(:default, RACK_ENV)
+
+get '/' do
+  erb :index
+end
+
+post '/txt' do
+  # https://www.twilio.com/docs/api/twiml
+  response = Twilio::TwiML::Response.new do |r|
+    r.Sms "Not implemented yet bro."
+  end
+
+  response.text
+end
+
+get '/voice' do
+  # http://twilio-ruby.readthedocs.org/en/latest/usage/twiml.html
+  response = Twilio::TwiML::Response.new do |r|
+    r.play "/mp3/hey.m4a"
+  end
+
+  response.text
+end
