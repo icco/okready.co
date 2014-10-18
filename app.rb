d6a7908c1cb8c81dc210224e35cdb746a9b4b4b8
Nat Welch <nat@natwelch.com>
2014-10-18 12:44:31 +0000

    configure db

--- app.rb
+++ app.rb
@@ -2,14 +2,22 @@
 
 # App Setup - Gems, etc.
 RACK_ENV ||= ENV['RACK_ENV'] ||= 'development' unless defined?(RACK_ENV)
+DATABASE_URL ||= ENV['DATABASE_URL'] ||= 'postgres://localhost/okready'
+
+# Gems
 require 'rubygems' unless defined?(Gem)
 require 'bundler/setup'
 Bundler.require(:default, RACK_ENV)
 
+# App Configuration
+set :database, DATABASE_URL
+
+# Root Handler
 get '/' do
   erb :index
 end
 
+# Twilio SMS endpoint
 post '/txt' do
   # https://www.twilio.com/docs/api/twiml
   response = Twilio::TwiML::Response.new do |r|
@@ -20,6 +28,7 @@ post '/txt' do
   response.text
 end
 
+# Twilio Voice Endpoint
 get '/voice' do
   # http://twilio-ruby.readthedocs.org/en/latest/usage/twiml.html
   response = Twilio::TwiML::Response.new do |r|
